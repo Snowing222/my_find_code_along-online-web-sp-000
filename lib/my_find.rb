df9c9176170df1d3fe6ad4b20fdf090b2b4ac290
elsa chen <snowing222@gmail.com>
2020-05-18 14:00:15 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -4,11 +4,10 @@ def my_find(collection)
   i=0
   while i<collection.length
     if yield(collection[i])
-      collection[i]
       break
     else
       i+=1
     end
   end
-
-end
+  collection[i]
+end 
